73ef114a3be1776f64ba5539277ed0dfe02893f8
abood2233ab <abdoza855@gmail.com>
2025-12-04 23:10:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// صوت عند الضغط على الأزرار
 function playClick() {
     let sound = document.getElementById("clickSound");
     sound.currentTime = 0;
     sound.play();
+}
+
+// تشغيل / إيقاف الموسيقى
+let musicPlaying = false;
+
+function toggleMusic() {
+    let music = document.getElementById("bgMusic");
+
+    if (!musicPlaying) {
+        music.play();
+        musicPlaying = true;
+    } else {
+        music.pause();
+        musicPlaying = false;
+    }
 }
